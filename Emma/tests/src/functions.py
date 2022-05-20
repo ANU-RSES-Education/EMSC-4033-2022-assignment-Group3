70576564a4ee6d3fc59d90c87ec0536285e31dd2
emma fitzgerald <u6669055@anu.edu.au>
2022-05-20 00:32:19 +0000

    finalised test_functions

--- Emma/tests/src/functions.py
+++ Emma/tests/src/functions.py
@@ -1,15 +1,71 @@
+"""All the functions we need to make a map:
+
+    - my_documentation()
+    - my_coastlines()
+    - my_water_features()
+    - my_basemaps()
+
+"""
+
 from .dependencies import *
+
+def my_documentation():
+
+    markdown_documentation = """   
+# Haec progressio est tabula faciens
+    
+## Moderni progressio programmandi 
+
+Moderni programmandi rationem habet organicam, accumsan sentientem, ut mirum inveniat. 
+In hoc cursu discimus quomodo per laminis perlegere discamus quomodo programmata aedificent et 
+quomodo nostra edificent. Incipiamus cum aliquibus praecipuis quaestionibus. 
+
+## Quid computatorium ? 
+
+Computatorium classicum est machina alicuius generis ad informationes 
+puras expediendas. Varia elementa opus sunt ad hoc possibilis efficiendum, 
+inter quas aliqua instrumenta initialisendi et recondendi informationes ante 
+et post discursum est, et ma- china ad informationes expediendas (including casus 
+ubi processus pendet ab ipsa informatione). Multae variae machinis his criteriis 
+occurrere possunt, sed plerumque unam saltem exigentiam adiungimus:
+
+## Aequationes mathematicae 
+
+Per "Navier-Stokes" datae sunt
+
+$$
+    \\frac{D \\mathbf{u}}{Dt} -\\nabla \cdot \\eta \\left( \\nabla \mathbf{u} + 
+    \\nabla \mathbf{u}^T \\right) - \\nabla p = \cdots
+$$
+
+
+## `Python` documentum
+
+Python hic est aliquis codicem quem animum advertere volumus
+
+```python
+# The classic "hello world" program
+print("salve mundi !")
+```
+"""
+    
+    return markdown_documentation
+
+
 
 def my_coastlines(resolution):
     """ returns the relevant coastlines at the requested resolution
         resolution should be one of '10m', '50m', or '110m' """
     import cartopy.feature as cfeature
 
-    
+# "Features" such as land, ocean, coastlines (50m =  the 1:50 million scale)
+
     return cfeature.NaturalEarthFeature('physical', 'coastline', '50m',
                                         edgecolor=(0.0,0.0,0.0),
                                         facecolor="none")
 
+
+    """fixed typo 'physical' and added resolution of 50m to c.feature.NaturalEarthFeature set """
 
 def my_water_features(resolution, lakes=True, rivers=True, ocean=False):
     """Returns a [list] of cartopy features"""
@@ -31,28 +87,60 @@ def my_water_features(resolution, lakes=True, rivers=True, ocean=False):
     
     return features
 
-
+    """ append adds to the end of the list, defined the features in water_features
+    ocean=False to remove the oceans from being read/displayed in the maps
+    added cfeature.NaturalEarthFeature specifications to each water body feature """
 
 def my_basemaps():
     """Returns a dictionary of map tile generators that cartopy can use"""
+    
+    ## The full list of available interfaces is found in the source code for this one:
+    ## https://github.com/SciTools/cartopy/blob/master/lib/cartopy/io/img_tiles.py
+
+    # dictionary of possible basemap tile objects
 
     mapper = {}
 
+#     ## Mapquest satellite / streetmap images 
+#     mapbox_outdoors = cimgt.MapQuestOSM()
+
+    ## Open Street map
     mapper["basemap_name"] = cimgt.OSM()
+
+    #changed to "basemap_name" in my_functions and in MapMaker.ipynb
+
+
+#     ## Mapbox Satellite images 
+
+#     mapbox_satellite = cimgt.MapboxTiles(map_id='satellite', access_token='pk.eyJ1IjoibG91aXNtb3Jlc2kiLCJhIjoiY2pzeG1mZzFqMG5sZDQ0czF5YzY1NmZ4cSJ9.lpsUzmLasydBlS0IOqe5JA')
+
+#     ## Google maps image tiles ()
+#     google_maps_street = cimgt.GoogleTiles(style="street") 
+#     google_maps_satellite = cimgt.GoogleTiles(style="satellite") 
+#     google_maps_terrain = cimgt.GoogleTiles(style="terrain") 
+
+#     ax.add_image(map_tiles, 11)
+#     ax.add_feature(coastline, linewidth=1.5,  edgecolor="Black", zorder=1, alpha=0.5)
+#     ax.add_feature(rivers,    linewidth=1.0,  edgecolor="Blue",  zorder=2, alpha=0.5)
 
     return mapper
 
 
+# # specify some point data (e.g. global seismicity in this case)
 
 def download_point_data(region):
     
-    from obspy.core import event
+    # from obspy.core import event
     from obspy.clients.fdsn import Client
     from obspy import UTCDateTime
 
     client = Client("IRIS")
 
     extent = region
+
+    """region is defined in MapMaker as the map_extent? The code for cat=... did not work with map_extent,    
+    however, is working with region. Maybe check if the postions in region (i.e. region[x]) allign with the 
+    map_extent long and lats. """
 
     starttime = UTCDateTime("1975-01-01")
     endtime   = UTCDateTime("2022-01-01")
@@ -64,8 +152,15 @@ def download_point_data(region):
                         maxlatitude=region[3],
                         minmagnitude=5.5, catalog="ISC")
     
-    
+    """ Defined the starttime and endtime as marked above, defined max and min longitude and latitude as the 
+    map_extent (defined by region), and made the catalog "ISC". Not sure what the minmagnitude is meant to be so 
+    I have left it at 5.5 """    
+
+
     print ("Point data: {} events in catalogue".format(cat.count()))
+
+
+    # Unpack the obspy data into a plottable array
 
     event_count = cat.count()
 
@@ -78,8 +173,11 @@ def download_point_data(region):
         eq_origins[ev,3] = dict(event.magnitudes[0])['mag']
         eq_origins[ev,4] = (dict(event.origins[0])['time']).date.year
 
-    return eq_origins
+    """changed the event_count from 4 to 5, however if it is meant to be 4 I am not sure if something needs to 
+     be removed, or if it's accounting for the last event labelled as '4'. Also added in some_code for the event 
+     specifications for lat, long, depth, mag and time """
 
+    return eq_origins
 
 
 def my_point_data(region):
@@ -88,15 +186,27 @@ def my_point_data(region):
     
     return data
 
-
+# global_extent     = [-180.0, 180.0, -90.0, 90.0]
+# # - Some global raster data (lon, lat, data) global plate age, in this example
 
 def download_raster_data():
     
+    # Seafloor age data and global image - data from Earthbyters
+
+    # The data come as ascii lon / lat / age tuples with NaN for no data. 
+    # This can be loaded with ...
+
+    # age = numpy.loadtxt("Resources/global_age_data.3.6.xyz")
+    # age_data = age.reshape(1801,3601,3)  # I looked at the data and figured out what numbers to use
+    # age_img  = age_data[:,:,2]
+
+    # But this is super slow, so I have just stored the Age data on the grid (1801 x 3601) which we can reconstruct easily
 
     from cloudstor import cloudstor
     teaching_data = cloudstor(url="L93TxcmtLQzcfbk", password='')
     teaching_data.download_file_if_distinct("global_age_data.3.6.z.npz", "Resources/global_age_data.3.6.z.npz")
 
+# added "Resources" to the teaching_data to match below in ages
     datasize = (1801, 3601, 3)
     raster_data = np.empty(datasize)
 
@@ -111,8 +221,9 @@ def download_raster_data():
     raster_data[...,1] = arrlats[...]
     raster_data[...,2] = ages[...]
 
-    return raster_data
+    """ added in specifications for age data (i.e. raster_data): ages, lats, lons, meshgrid """
 
+    return raster_data
 
 
 def my_global_raster_data():
@@ -120,5 +231,4 @@ def my_global_raster_data():
     raster = download_raster_data()
     
     return raster
-
 
--- Emma/tests/test_functions.py
+++ Emma/tests/test_functions.py
@@ -1,70 +1,57 @@
-# import pytest
-# from src.functions import *
-
-# def test_foo_function(rtol=1.e-13):
-#    result = foo_function(4) - 16
-#    assert result < rtol, " *** error is too big "
-
-
 import pytest
 #from src import my_functions
 from src.functions import *
 
-# +
-# def test_my_water_features():
-    
-#     features = my_water_features(resolution = '10m')
-    
-#     assert len(features) == 2, "only lakes and rivers whould be listed, so a list of 2 features"
 
-# #tested how many resolutions of '10m' are in the function, which should be two as only rivers and lakes are defined as True.
-# #test that the three features are being imput into the function and the output is lakes=True, rivers=True, ocean=False at the correct resolution
+def test_my_water_features():
 
-# +
-# def test_my_basemaps():
-    
-#     result = my_basemaps()
-#     assert len(result) == 1, "mapper should return a single dictionary of map tiles for cartopy to use"
-    
-# # Returns a dictionary of map tile generators that cartopy can use
-# +
-# def test_my_coastlines():
-    
-#     result = my_coastlines(resolution = '50m')
-    
-#     assert type(result) != None, "result should be an cartopy object"
+    features = my_water_features(resolution = '10m')
+    assert len(features) == 2, "only lakes and rivers whould be listed, so a list of 2 features"
 
-# # "type = {}".format(type(result)) 
-# #returns the relevant coastlines at the requested resolution
-# -
+    #tested how many resolutions of '10m' are in the function, which should be two as only rivers and lakes are defined as True. #test that the three features are being imput into the function and the output is lakes=True, rivers=True, ocean=False at the correct resolution
 
+
+def test_my_basemaps():
+
+    result = my_basemaps()
+    assert len(result) == 1, "mapper should return a single dictionary of map tiles for cartopy to use"
+    # Returns a dictionary of map tile generators that cartopy can use
+
+
+# region
+def test_my_coastlines():
+
+    result = my_coastlines(resolution = '50m')
+    assert type(result) != None, "result should be an cartopy object"
+
+#"type = {}".format(type(result))
+#returns the relevant coastlines at the requested resolution
+# endregion
 
 
 def test_download_point_data():
     
-    result = download_point_data(region = [1, 2, 3, 4])
+    result = download_point_data(region)
+    assert type(result) == numpy.ndarray
+
+
+# region
+def test_download_raster_data():
     
-    assert result.shape == (132, 5)
+    result = download_raster_data()
+    assert result.shape == (1801, 3601, 3)
 
-# ! pytest
+# test whether the raster data is being downloaded from cloudstor and that the raster data is being specified
 
-# +
-# def test_download_raster_data():
-    
-#     result = download_raster_data()
-    
-#     assert result.shape == (1801, 3601, 3)
+# endregion
 
-# # test whether the raster data is being downloaded from cloudstor and that the raster data is being specified
 
-# +
+# region
 # def test_download_raster_data():
     
 #     result_lats = download_raster_data(x = [1])
-    
 #     assert result_lat == all(-90 <= x <= 90, [1])
 
-# +
-#     with pytest.warns(UserWarning):
-#     my_coastlines()
-
+    """ I was trying to make the test specific to spatial points in the array, such as testing whether 
+    the second coloumn (latitude) is within the boundaries of -90 to 90. I couldn't get it to work"""
+# endregion
